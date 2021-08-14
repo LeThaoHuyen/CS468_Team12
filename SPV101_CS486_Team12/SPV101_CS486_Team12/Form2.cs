4f02443d06dc60be877876f48bf008552f5e741c
Huyen Le <lethaohuyen3010@gmail.com>
2021-08-14 09:50:58 +0700

    trial

--- SPV101_CS486_Team12/SPV101_CS486_Team12/Form2.cs
+++ SPV101_CS486_Team12/SPV101_CS486_Team12/Form2.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Team12
 {
@@ -15,6 +17,61 @@ namespace Team12
         public Form2()
         {
             InitializeComponent();
+        }
+
+        string[] officials = new string[6];
+        string[] reverses = new string[6];
+
+        public void loadOfficial()
+        {
+            string query = "sp_selectprincipal";
+            SqlConnection connection = null;
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString.ToString();
+           
+            try
+            {
+                DataSet data = Database.ExecuteQuery(connection, query, connectionString, CommandType.StoredProcedure);
+                //dataGridViewTrial.Columns["Official"].Add = data.Tables[0];
+                for (int i = 0; i < 6; i++)
+                {
+                    officials[0] = data.Tables[0].Rows[0].ToString();
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                MessageBox.Show(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void loadReverse()
+        {
+            string query = "sp_selectprincipal";
+            SqlConnection connection = null;
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString.ToString();
+
+            try
+            {
+                DataSet data = Database.ExecuteQuery(connection, query, connectionString, CommandType.StoredProcedure);
+                //dataGridViewTrial.Columns["Official"].Add = data.Tables[0];
+                for (int i = 0; i < 6; i++)
+                {
+                    reverses[0] = data.Tables[0].Rows[0].ToString();
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                MessageBox.Show(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
---
+++ SPV101_CS486_Team12/SPV101_CS486_Team12/FormTrial.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Team12
+{
+    partial class FormTrial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewTrial = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Song = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTrial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTrial
+            // 
+            this.dataGridViewTrial.AllowUserToAddRows = false;
+            this.dataGridViewTrial.AllowUserToDeleteRows = false;
+            this.dataGridViewTrial.AllowUserToResizeColumns = false;
+            this.dataGridViewTrial.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            this.dataGridViewTrial.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewTrial.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewTrial.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridViewTrial.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewTrial.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridViewTrial.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(45)))), ((int)(((byte)(54)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Gold;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Gold;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTrial.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewTrial.ColumnHeadersHeight = 60;
+            this.dataGridViewTrial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridViewTrial.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Song,
+            this.Result});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(49)))), ((int)(((byte)(61)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Goldenrod;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTrial.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewTrial.EnableHeadersVisualStyles = false;
+            this.dataGridViewTrial.GridColor = System.Drawing.Color.Beige;
+            this.dataGridViewTrial.Location = new System.Drawing.Point(46, 109);
+            this.dataGridViewTrial.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridViewTrial.Name = "dataGridViewTrial";
+            this.dataGridViewTrial.ReadOnly = true;
+            this.dataGridViewTrial.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewTrial.RowHeadersVisible = false;
+            this.dataGridViewTrial.RowHeadersWidth = 62;
+            this.dataGridViewTrial.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Goldenrod;
+            this.dataGridViewTrial.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridViewTrial.RowTemplate.DefaultCellStyle.Padding = new System.Windows.Forms.Padding(10);
+            this.dataGridViewTrial.RowTemplate.Height = 40;
+            this.dataGridViewTrial.RowTemplate.ReadOnly = true;
+            this.dataGridViewTrial.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTrial.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridViewTrial.ShowCellErrors = false;
+            this.dataGridViewTrial.ShowCellToolTips = false;
+            this.dataGridViewTrial.ShowEditingIcon = false;
+            this.dataGridViewTrial.ShowRowErrors = false;
+            this.dataGridViewTrial.Size = new System.Drawing.Size(764, 360);
+            this.dataGridViewTrial.TabIndex = 15;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Official";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Reverse";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Song
+            // 
+            this.Song.HeaderText = "Song";
+            this.Song.MinimumWidth = 8;
+            this.Song.Name = "Song";
+            this.Song.ReadOnly = true;
+            this.Song.Width = 150;
+            // 
+            // Result
+            // 
+            this.Result.HeaderText = "Best performances";
+            this.Result.MinimumWidth = 8;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Width = 150;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label2.Location = new System.Drawing.Point(46, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 50);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Team List";
+            // 
+            // FormTrial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 522);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridViewTrial);
+            this.Name = "FormTrial";
+            this.Text = "FormTrial";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTrial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTrial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Song;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Result;
+        private System.Windows.Forms.Label label2;
+    }
+}
